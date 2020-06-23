ddc800a393717ef8562407c5a4d9f62cb7a3f87c
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-23 17:23:00 +0200

    pictures list with using Grid

--- src/components/skomponuj.js
+++ src/components/skomponuj.js
@@ -10,11 +10,16 @@ function Skomponuj() {
     }, [])
 
     return (
-        <div>
+        <div className="dodatki">
             <h1>Skomponuj pizzę</h1>
-            {Dodatki.map((dodatkek) => {
+            {Dodatki.map((dodatek, index) => {
                 return (
-                    <p>{dodatkek.nazwa}</p>
+                    <div key={index} className="dodatekRzad">
+                        <input type="checkbox" />
+                        <img className="dodatek_ikonka" src={process.env.PUBLIC_URL + "/assets/" + dodatek.nazwa + ".png"} alt={dodatek.nazwa} />
+                        <p>{dodatek.nazwa}</p>
+                        {dodatek.koszt === 0 ? <p>free</p> : <p>{(dodatek.koszt / 100).toFixed(2)}zł</p>}
+                    </div>
                 )
             })}
         </div>
